ea85973b84578645e9aa9bb40f848dcdaab7615c
NASKK <thomas.berenger76@gmail.com>
2023-05-15 21:41:48 +0200

    0.0.0
    init - structure laravel, installation de breeze,tailwind

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|unique:tasks',
+            'description' => 'required',
+        ]);
+
+        Task::create($validatedData);
+
+        return redirect()->route('tasks.index')->with('success', 'La tâche a été ajoutée avec succès.');
+    }
+
+    public function edit(Task $task)
+    {
+        return view('tasks.edit', compact('task'));
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|unique:tasks,name,' . $task->id,
+            'description' => 'required',
+        ]);
+
+        $task->update($validatedData);
+
+        return redirect()->route('tasks.index')->with('success', 'La tâche a été modifiée avec succès.');
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return redirect()->route('tasks.index')->with('success', 'La tâche a été supprimée avec succès.');
+    }
+}
---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->randomElement(['Finir dashboard', 'Faire un test', 'Finir css', 'Finir produits']),
+            'description' => fake()->randomElement(['description1', 'description2', 'description3', 'description4', 'description5', ]),
+
+            //
+        ];
+    }
+}
